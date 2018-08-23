6e6c13f2305d9519ae33309183af2509d991a5f8
Zoran Nikolic <znikolic@croz.net>
2018-08-23 21:20:14 +0200

    Added homepage controller & gsp

---
+++ grails-app/controllers/ba/giz/HomepageController.groovy
@@ -0,0 +1,31 @@
+package ba.giz
+
+import grails.transaction.Transactional
+
+import static org.springframework.http.HttpStatus.NOT_FOUND
+
+@Transactional(readOnly = true)
+class HomepageController {
+
+  static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+  def homepage() {
+    //Find preduzece by current user
+    //Find Izvjestaj list by current user (or by  preduzece)
+    //Mocked for now
+    def preduzece = Preduzece.last() ? Preduzece.last() : new Preduzece()
+    def izvjestajList = Izvjestaj.findAll()
+    respond preduzece, model: [list: izvjestajList, properties: ["tip", "podaciPodnosenjeIzvjestaja.godina", "datumKreiranja",
+                                                                 "datumSlanja", "podaciPodnosenjeIzvjestaja.displayName", "status"]]
+  }
+
+  protected void notFound() {
+    request.withFormat {
+      form multipartForm {
+        flash.message = message(code: 'default.not.found.message', args: [message(code: 'izvjestaj.label', default: 'Book'), params.id])
+        redirect action: "index", method: "GET"
+      }
+      '*' { render status: NOT_FOUND }
+    }
+  }
+}
